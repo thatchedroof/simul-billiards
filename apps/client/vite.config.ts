60057f8883b882290d9c16f96736b46ff1620ae8
thatchedroof <51761205+thatchedroof@users.noreply.github.com>
2026-04-10 01:07:56 -0700

    Update vite.config.ts

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
 
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/siturbi/" : "/",
+  base: mode === "production" ? "/simul-billiards/" : "/",
 }));
